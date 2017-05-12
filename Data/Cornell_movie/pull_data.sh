d73f632525eaa01c3e89d206a87b02ea82eaca73
alexhsu <b02901053@ntu.edu.tw>
2017-05-12 19:48:20 +0800

    cornell movie corpus

---
+++ Data/Cornell_movie/pull_data.sh
@@ -0,0 +1,4 @@
+wget -c 'https://www.dropbox.com/s/tbp1m7uwyuloiyn/corpus_lstm.zip?dl=0' -O corpus_lstm.zip
+wget -c 'https://www.dropbox.com/s/mqghwq5kyo67j50/corpus_seq2seq.zip?dl=0' -O corpus_seq2seq.zip
+wget -c 'https://www.dropbox.com/s/7kx8cvy0ipcfl7a/normal_form_for_seq2seq.zip?dl=0' -O normal_form_for_seq2seq.zip
+wget -c 'https://www.dropbox.com/s/1c9vu4tms47g0uk/original_corpus.zip?dl=0' =O original_corpus.zip

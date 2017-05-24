7bab424b6bddcaf8e5706d96cb6872243b6ead01
alexhsu <b02901053@ntu.edu.tw>
2017-05-24 15:24:20 +0800

    asr data

---
+++ Data/Cornell_movie/asr_data/pull_data.sh
@@ -0,0 +1,3 @@
+wget -c 'https://www.dropbox.com/s/o35qk714nliq6g2/normal_form_for_seq2seq_asr.zip?dl=0' -O normal_form_for_seq2seq_asr.zip
+unzip normal_form_for_seq2seq_asr.zip
+rm normal_form_for_seq2seq_asr.zip
--- Data/Cornell_movie/original_data/pull_data.sh
+++ Data/Cornell_movie/original_data/pull_data.sh
@@ -1,5 +1,19 @@
 wget -c 'https://www.dropbox.com/s/tbp1m7uwyuloiyn/corpus_lstm.zip?dl=0' -O corpus_lstm.zip
+unzip corpus_lstm.zip
+rm corpus_lstm.zip
+
 wget -c 'https://www.dropbox.com/s/mqghwq5kyo67j50/corpus_seq2seq.zip?dl=0' -O corpus_seq2seq.zip
+unzip corpus_seq2seq.zip
+rm corpus_seq2seq.zip
+
 wget -c 'https://www.dropbox.com/s/7kx8cvy0ipcfl7a/normal_form_for_seq2seq.zip?dl=0' -O normal_form_for_seq2seq.zip
+unzip normal_form_for_seq2seq.zip
+rm normal_form_for_seq2seq.zip
+
 wget -c 'https://www.dropbox.com/s/1c9vu4tms47g0uk/original_corpus.zip?dl=0' -O original_corpus.zip
+unzip original_corpus.zip
+rm original_corpus.zip
+
 wget -c 'https://www.dropbox.com/s/cwfrs2soorw5vj7/normal_form_for_seq2seq_utf8.zip?dl=0' -O normal_form_for_seq2seq_utf8.zip
+unzip normal_form_for_seq2seq_utf8.zip
+rm normal_form_for_seq2seq_utf8.zip

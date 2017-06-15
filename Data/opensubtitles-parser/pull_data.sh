0cc8b80878fe27243f2d9266a9559f104e6040ac
alexhsu <b02901053@ntu.edu.tw>
2017-06-16 00:24:09 +0800

    renew data:

--- Data/opensubtitles-parser/pull_data.sh
+++ Data/opensubtitles-parser/pull_data.sh
@@ -1,3 +1,3 @@
-wget -c 'https://www.dropbox.com/s/5wufesyy69821s8/opensubtitle_parsed.zip?dl=0' -O opensubtitle_parsed.zip
+wget -c 'https://www.dropbox.com/s/g6m3j4rkm5rhxqm/opensubtitle_parsed.zip?dl=0' -O opensubtitle_parsed.zip
 unzip opensubtitle_parsed.zip
 rm opensubtitle_parsed.zip

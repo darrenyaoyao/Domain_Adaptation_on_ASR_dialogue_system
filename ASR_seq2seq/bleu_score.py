e90ddc6980ee52fda4775c2d2b50219af044f8a4
alexhsu <b02901053@ntu.edu.tw>
2017-06-26 17:20:45 +0800

    modify bleu_score by consider lowercase and 標點符號

--- ASR_seq2seq/bleu_score.py
+++ ASR_seq2seq/bleu_score.py
@@ -1,23 +1,36 @@
 import nltk
 import argparse
-from nltk.translate.bleu_score import SmoothingFunction
+import re
 
-chencherry = SmoothingFunction()
+_WORDS_SPLIT = re.compile("([.,!?\"':;)(])") 
+def basic_tokenizer(sentence):
+    words = [ ]
+    for space_separated_fragment in sentence.strip().split():
+        words.extend(_WORDS_SPLIT.split(space_separated_fragment))
+    return [w.lower() for w in words if w]
 
 parser = argparse.ArgumentParser(description=
                                  'Calculate belu score of original \
                                   predict data and ASR predict data')
 parser.add_argument('--original', dest='original')
 parser.add_argument('--asr', dest='asr')
+parser.add_argument('--lines', dest="lines")
 
 args = parser.parse_args()
 original = open(args.original)
 asr = open(args.asr)
+f_line = open(args.lines)
+ori_lines = [ ]
+for ori_line in f_line:
+    ori_lines.append(int(ori_line) -1 )
 
 original_data = []
 asr_data = []
-for line in original:
-    original_data.append(line)
+for counter,line in enumerate(original):
+    if counter in ori_lines:
+        tokens = basic_tokenizer(line)
+        temp = " ".join(tokens)
+        original_data.append(temp)
 
 for line in asr:
     asr_data.append(line)
@@ -26,7 +39,6 @@ BLEUscore = 0
 for i in range(len(original_data)):
     hypothesis = asr_data[i]
     reference = original_data[i]
-    BLEUscore += nltk.translate.bleu_score.sentence_bleu([reference], hypothesis, \
-                                                         smoothing_function=chencherry.method2)
+    BLEUscore += nltk.translate.bleu_score.sentence_bleu([reference], hypothesis)
 
 print(BLEUscore/len(original_data))
